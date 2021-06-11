39b07757c4ef21b7022b6896242b4172aeeabb80
Paul Anderson <paul@andersonpaul.com>
2021-06-10 20:28:49 -0500

    chore: fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -19,22 +19,25 @@ test("renders Loading", () => {
 });
 
 test("renders title", () => {
-  useAsync.mockReturnValue({ isPending: false, result: [{ foo: "bar" }] });
+  useAsync.mockReturnValue({ isPending: false, result: [{ WMI: "foo" }] });
   render(<App />);
   const titleElement = screen.getByText(/WMI Data - Honda/i);
   expect(titleElement).toBeInTheDocument();
 });
 
 test("renders wmi data", () => {
-  useAsync.mockReturnValue({ isPending: false, result: [{ Country: "USA" }] });
+  useAsync.mockReturnValue({
+    isPending: false,
+    result: [{ WMI: "foo", Country: "USA" }],
+  });
   render(<App />);
   const linkElements = screen.getAllByText(/usa/i);
   expect(linkElements.length).toBe(1);
 });
 
 test("shows length > 0", () => {
-  useAsync.mockReturnValue({ isPending: false, result: [{ foo: "bar" }] });
+  useAsync.mockReturnValue({ isPending: false, result: [{ WMI: "foo" }] });
   render(<App />);
-  const linkElement = screen.getByText(/total\:\s+[0-9]/i);
+  const linkElement = screen.getByText(/total:\s+[0-9]/i);
   expect(linkElement).toBeInTheDocument();
 });
