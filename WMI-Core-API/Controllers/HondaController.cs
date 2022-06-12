c446758270fc70efe9925ef797389a683142053e
Leo Delos Reyes <leodcdelosreyes@live.com>
2022-06-12 09:15:36 -0500

    API Project initial setup

---
+++ WMI-Core-API/Controllers/HondaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WMI_Core_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HondaController : ControllerBase
+    {
+    }
+}
